1d2a1516a971e8c9dd1d62d27e9f4a091c613ab7
madbullahi <mabdullahi259@protonmail.com>
2022-12-28 23:43:45 +0000

    commit

---
+++ codes/script.R
@@ -0,0 +1,15 @@
+# The directory where the counts for each samples are located.
+
+data_dir <- 'salmon'
+  
+# The sample file must be in CSv format and must have the headers "sample".
+
+desing_file = "design.csv"
+
+# What software created the mappings.
+
+method <- "salmon"
+
+# The output file name.
+
+output_file = "transript_counts.csv"
